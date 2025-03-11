4d5902700c7e8bd574dd34be31a3426a74643b77
Trevor Karuma <trevormukundi@gmail.com>
2025-03-12 01:33:13 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []; 
+
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return messages; 
+}
+const namesArray = ["Charlie", "Samip", "Ali"];
+const eventName = "birthday";
+console.log(writeCards(namesArray, eventName));
+
+
+function countDown (countNum) {
+    while (countNum >= 0){
+        console.log(countNum);
+        countNum--;
+    }
+}
+
+countDown(10)
